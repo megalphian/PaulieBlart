e6a11f262590b27ea8884bffe09e8f2d19518202
Megnath Ramesh <megatron@Megnaths-MacBook-Pro.local>
2018-04-04 16:49:37 -0600

    Refined dataset for training. Last classifier sucked

--- scripts/train.py
+++ scripts/train.py
@@ -1,18 +1,19 @@
 import json
+import os
 from watson_developer_cloud import VisualRecognitionV3
 
 visual_recognition = VisualRecognitionV3(
-    '2016-05-20',
-    api_key='fdbed6c3c7053723edbcdbc1259bc96e97b14c4e')
+    '2018-04-04',
+    api_key='8ba18ee276c2d712803ab3ef658403c965bf12ce')
 
-print('before slow json upload')
+data_dir = "../Dataset"
 
-with open('frank.zip', 'rb') as frank,\
-    open('dan.zip', 'rb') as dan:
+with open(os.path.join(data_dir, "Megnath.zip"), 'rb') as meg,\
+    open(os.path.join(data_dir, "negative.zip"), 'rb') as negative:
     model = visual_recognition.create_classifier(
         'peopleRecognition',
-        frank_positive_examples=frank,
-        dan_positive_examples=dan)
+        megnath_positive_examples=meg,
+        negative_examples=negative)
 
 with open('results.txt', 'w') as outfile:
     json.dump(model, outfile, indent=2)
